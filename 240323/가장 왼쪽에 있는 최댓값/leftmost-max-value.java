35bb91c032c814178877623990519eb2a0a7342d
김도환 <87745117+coliny123@users.noreply.github.com>
2024-03-23 01:05:00 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 258ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240323/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[]arr = new int [n];
+        int idx = -1;
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+
+        while(idx+1 != 1){
+            for(int i=0; i<n; i++){
+                if(arr[i] > max){
+                    max = arr[i];
+                    idx = i;
+                }
+            }
+            System.out.print(idx+1 + " ");
+            n = idx;
+            max = Integer.MIN_VALUE;
+        }
+
+
+
+    }
+}
