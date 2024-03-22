578cbaf4cecd1038bd5688b05bab7d0c58d50e76
김도환 <87745117+coliny123@users.noreply.github.com>
2024-03-23 02:46:21 +0900

    Title: 두 숫자의 차의 최솟값, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240323/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
+++ 240323/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -6,17 +6,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[]arr = new int[n];
-        int min = Integer.MAX_VALUE;
 
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
 
-        for(int i=1; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(min > arr[j]-arr[i-1]){
-                    min = arr[j]-arr[i-1];
-                }
+        int min = arr[1] - arr[0];
+
+        for(int i=2; i<n; i++){
+            if(min > arr[i] - arr[i-1]){
+                min = arr[i] - arr[i-1];
             }
         }
 
