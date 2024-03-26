05e30b3d94e2354911104ee18c20c917a16be4d9
김도환 <87745117+coliny123@users.noreply.github.com>
2024-03-27 02:17:59 +0900

    Title: Run Length 인코딩, Time: 130ms, Memory: 11MB, Status: Passed - CodeTree

--- 240327/Run Length 인코딩/run-length-encoding.java
+++ 240327/Run Length 인코딩/run-length-encoding.java
@@ -9,26 +9,26 @@ public class Main {
 
         char before = str.charAt(0);
         int cnt = 0;
-        StringBuilder sb = new StringBuilder();
+        String result = "";
 
 
         for(int i=0; i<len; i++){
             if(str.charAt(i) == before){
                 cnt++;
             }else{
-                sb.append(before);
-                sb.append(cnt);
+                result += before;
+                result += Integer.toString(cnt);
 
                 before = str.charAt(i);
                 cnt = 1;
             }
             if(i == len-1){
-                sb.append(before);
-                sb.append(cnt);
+                result += before;
+                result += Integer.toString(cnt);
             }
         }
 
-        System.out.println(sb.length());
-        System.out.println(sb.toString());
+        System.out.println(result.length());
+        System.out.println(result);
     }
 }
