9fb98480e13b184260bfd97ddd17a9e486f0a6fa
김도환 <87745117+coliny123@users.noreply.github.com>
2024-04-15 00:52:18 +0900

    Title: 특정 문자열을 출력하는 함수, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240415/특정 문자열을 출력하는 함수/function-that-outputs-specific-string.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            System.out.println("Hello");
+            System.out.println("#@#@#@#@#@");
+            System.out.println("CodeTree");
+            System.out.println("@#@#@#@#@#");
+            System.out.println("Students!");
+            System.out.println();
+        }
+    }
+}
