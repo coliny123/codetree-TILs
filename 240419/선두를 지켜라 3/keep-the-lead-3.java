2612fbbbabf78e1d8a9e691a0f8f730428263191
김도환 <87745117+coliny123@users.noreply.github.com>
2024-04-19 00:15:22 +0900

    Title: 선두를 지켜라 3, Time: 142ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240419/선두를 지켜라 3/keep-the-lead-3.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int A[] = new int[1000001];
+        int B[] = new int[1000001];
+
+        int timeA = 1;
+        for(int i=0; i<n; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            while(t-- > 0){
+                A[timeA] = A[timeA - 1] + v;
+                timeA++;
+            }
+        }
+
+        int timeB = 1;
+        for(int i=0; i<m; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            while(t-- > 0){
+                A[timeB] = A[timeB - 1] + v;
+                timeB++;
+            }
+        }
+
+        int sundo = 0, answer = 0;
+        for(int i = 1; i <=timeA; i++) {
+            if(A[i] > B[i]) {
+                if(sundo != 1)
+                    answer++;
+                sundo = 1; 
+            }
+            else if(A[i] < B[i]) {
+                if(sundo != 2)
+                    answer++;
+                sundo = 2; 
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
