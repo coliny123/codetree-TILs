a954452b146ac415f990ca5a3c50fa4764556efe
김도환 <87745117+coliny123@users.noreply.github.com>
2024-04-30 21:28:34 +0900

    Title: 작은 구슬의 이동, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240430/작은 구슬의 이동/small-marble-movement.java
+++ 240430/작은 구슬의 이동/small-marble-movement.java
@@ -1,48 +1,52 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class Main {
 
-    public static boolean inRange(int x, int y, int n){
-        return(0<=x && x<n && 0<=y && y<n);
+    public static boolean inRange(int r, int c, int n){
+        return(0<=r && r<n && 0<=c && c<n);
     }
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        
-        int dx[] = new int[]{0, 1, 0, -1};
-        int dy[] = new int[]{1, 0, -1, 0};
-
         int n = sc.nextInt();
         int t = sc.nextInt();
-        int r = sc.nextInt() - 1;
-        int c = sc.nextInt() - 1;
-        char d = sc.next().charAt(0);
 
+        int [] dx = new int[]{0, 1, -1, 0};
+        int [] dy = new int[]{1, 0, 0, -1};
+        int[][]arr2d = new int[n][n];
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        char d = sc.next().charAt(0);
         int dir = 0;
+
         if(d == 'U'){
-            dir = 0;
-        }else if(d == 'D'){
             dir = 2;
-        }else if(d == 'L'){
-            dir = 3;
-        }else{
+        }else if(d == 'D'){
             dir = 1;
+        }else if(d == 'R'){
+            dir = 0;
+        }else{
+            dir = 3;
         }
 
+        r -= 1;
+        c -= 1;
 
         for(int i=0; i<t; i++){
             int nx = r + dx[dir];
-            int ny = c + dy[dir];
-            if(inRange(nx, ny, n)){
+            int ny = r + dy[dir];
+            if(inRange(nx,ny,n)){
                 r = nx;
                 c = ny;
             }else{
-                dir = (dir + 2) % 4;
+                dir = 3 - dir;
             }
-        }
 
-        System.out.print((c+1) + " " +(r+1));
-        
+        }
+        System.out.print((r+1) + " " +(c+1));
+
+
     }
 }
