d31a57339a14057ff30e25e86a9e6e45f0d3ecd4
김도환 <87745117+coliny123@users.noreply.github.com>
2024-05-03 23:08:55 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 254ms, Memory: 16MB, Status: Passed - CodeTree

--- 240503/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
+++ 240503/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -2,60 +2,56 @@ import java.util.*;
 
 public class Main {
     public static int n;
+    public static int currX;
+    public static int currY;
     public static int arr[][];
-    public static int dx[] = new int[]{0, 0, 1, -1};
-    public static int dy[] = new int[]{1, -1, 0, 0};
+    public static int dx[] = new int[]{-1, 1, 0, 0};
+    public static int dy[] = new int[]{0, 0, -1, 1};
 
     public static boolean inRange(int x, int y){
         return(0<=x && x<n && 0<=y && y<n);
     }
-    public static class Pair{
-        int x, y;
-        public Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    public static void go(Pair pair){
+    public static ArrayList<Integer> visitedNums = new ArrayList<>();
 
+    public static boolean simulate(){
+        for(int i=0; i<4; i++){
+            int nx = currX + dx[i];
+            int ny = currY + dy[i];
+
+            if(inRange(nx, ny) && arr[nx][ny] > arr[currX][currY]){
+                currX = nx;
+                currY = ny;
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int r = sc.nextInt();
-        int c = sc.nextInt();
-
+        currX = sc.nextInt();
+        currY = sc.nextInt();
         arr = new int[n][n];
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 arr[i][j] = sc.nextInt();
             }
         }
-        r--; c--;
+        currX--; currY--;
 
-        Pair maxPair = new Pair(r, c);
-        int max = arr[r][c];
-        System.out.print(max + " ");
+        visitedNums.add(arr[currX][currY]);
         while(true){
-            int beforeMax = max;
-            for(int i=0; i<4; i++){
-                int nx = r + dx[i];
-                int ny = c + dy[i];
-                if(inRange(nx, ny) && arr[nx][ny] > beforeMax){
-                    max = arr[nx][ny];
-                    maxPair.x = nx;
-                    maxPair.y = ny;
-                }
-            }
-            r = maxPair.x;
-            c = maxPair.y;
-            if(beforeMax == max){
+            boolean greaterNumberExist = simulate();
+            if(!greaterNumberExist){
                 break;
             }
-            beforeMax = max;
-            System.out.print(max + " ");
+            visitedNums.add(arr[currX][currY]);
         }
+
+        for(int i = 0; i < visitedNums.size(); i++)
+            System.out.print(visitedNums.get(i) + " ");
     }
 }
