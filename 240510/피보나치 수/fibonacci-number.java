052bee292717f9830bab704d677bb86fde9b6c91
김도환 <87745117+coliny123@users.noreply.github.com>
2024-05-10 19:37:45 +0900

    Title: 피보나치 수, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240510/피보나치 수/fibonacci-number.java
+++ 240510/피보나치 수/fibonacci-number.java
@@ -8,9 +8,11 @@ public class Main {
         int dp[] = new int[n+1];
 
         dp[1] =1;
-        dp[2] =1;
-        for(int i=3; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        if(n >1){
+            dp[2] =1;
+            for(int i=3; i<=n; i++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
         }
         System.out.println(dp[n]);
     }
