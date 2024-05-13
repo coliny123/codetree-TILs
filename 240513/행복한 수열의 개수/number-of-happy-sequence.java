3e5819038a48a45954946b04b73cd2698eb197f3
김도환 <87745117+coliny123@users.noreply.github.com>
2024-05-13 21:15:35 +0900

    Title: 행복한 수열의 개수, Time: 307ms, Memory: 20MB, Status: Passed - CodeTree

--- 240513/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240513/행복한 수열의 개수/number-of-happy-sequence.java
@@ -1,62 +1,55 @@
 import java.util.*;
 
 public class Main {
+    public static int n;
+    public static int m;
+
+    public static int grid[][];
+
+    public static int seq[];
+
+    public static boolean isHappySequence(){
+        int consecutiveCount = 1, maxCcnt=1;
+        for(int i=1; i<n; i++){
+            if(seq[i-1] == seq[i])
+                consecutiveCount++;
+            else
+                consecutiveCount = 1;
+
+            maxCcnt = Math.max(maxCcnt, consecutiveCount);
+        }
+
+        return maxCcnt >= m;
+    }
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
         int answer = 0;
 
-        int grid[][] = new int[n][n];
+        grid = new int[n][n];
+        seq = new int[n];
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                grid[i][j] = sc.nextInt();
+                seq[j] = grid[i][j];
             }
+            if(isHappySequence())
+                answer++;
         }
 
-        for(int row=0; row<n; row++){
-            int cnt=1;
-            int now=grid[row][0];
-            if(n==1){
+        for(int j=0; j<n; j++){
+            for(int i=0; i<n; i++){
+                seq[i] = grid[i][j];
+            }
+            if(isHappySequence())
                 answer++;
-                break;
-            }
-            for(int col=1; col<n; col++){
-                if(now == grid[row][col]){
-                    cnt++;
-                }else{
-                    now = grid[row][col];
-                    cnt = 1;
-                }
-                if(cnt >= m){
-                    answer++;
-                    break;
-                }
-            }
-        }
-
-        for(int col=0; col<n; col++){
-            int cnt=1;
-            int now=grid[0][col];
-            if(n==1){
-                answer++;
-                break;
-            }
-            for(int row=1; row<n; row++){
-                if(now == grid[row][col]){
-                    cnt++;
-                }else{
-                    now = grid[row][col];
-                    cnt = 1;
-                }
-
-                if(cnt >= m){
-                    answer++;
-                    break;
-                }
-            }
         }
 
         System.out.println(answer);
