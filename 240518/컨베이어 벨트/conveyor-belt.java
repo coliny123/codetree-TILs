5314265211e7899a46ac62ab30b10183e7f0ee77
김도환 <87745117+coliny123@users.noreply.github.com>
2024-05-18 23:53:32 +0900

    Title: 컨베이어 벨트, Time: 198ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240518/컨베이어 벨트/conveyor-belt.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+
+        int arr[] = new int[2*n];
+
+        for(int i=0; i<2*n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+
+        while(t-- > 0){
+            int temp = arr[2*n-1];
+            for(int i=2*n-1; i>=1; i--){
+                arr[i] = arr[i-1];
+            }
+            arr[0] = temp;
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        for(int i=n; i<2*n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+
+    }
+}
