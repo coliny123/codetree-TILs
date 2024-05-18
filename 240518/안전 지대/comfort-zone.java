a012b7c5107b75a893c3e20b0a244523a9a842fa
김도환 <87745117+coliny123@users.noreply.github.com>
2024-05-18 19:40:18 +0900

    Title: 안전 지대, Time: 108ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240518/안전 지대/comfort-zone.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int max = Integer.MIN_VALUE;
+    public static int cnt;
+    public static int graph[][];
+    public static int answer[];
+    public static boolean visited[][];
+    public static ArrayList<Integer> vilage = new ArrayList<>();
+
+    public static void print(){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(visited[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+
+    public static void reset(){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                visited[i][j] = false;
+            }
+        }
+    }
+
+    public static boolean inRange(int x, int y){
+        return (0<=x && x<n && 0<=y && y<m);
+    }
+
+    public static boolean canGo(int x, int y, int k){
+        if(!inRange(x, y)){
+            return false;
+        }
+
+        if(visited[x][y] || graph[x][y] <= k){
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public static void DFS(int x, int y, int k){
+        int dx[] = new int[]{1,0,-1,0};
+        int dy[] = new int[]{0,-1,0,1};
+
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(canGo(nx, ny, k)){
+                visited[nx][ny] = true;
+                DFS(nx, ny, k);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        graph = new int[n][m];
+        visited = new boolean[n][m];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                int a = sc.nextInt();
+                graph[i][j] = a;
+                max = Math.max(max, a);
+            }
+        }
+
+        answer = new int[max+1];
+
+        for(int k=1; k<=max; k++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    if(canGo(i, j, k)){
+                        visited[i][j] = true;
+                        DFS(i, j, k);
+                        vilage.add(1);
+                    }
+                }
+            }
+            answer[k] = vilage.size();
+            vilage.clear();
+            // System.out.println(k);
+            // print();
+            reset();
+        }
+
+        int maxVilage = answer[1];
+        int idx = 1;
+        for(int k=2; k<=max; k++){
+            if(answer[k] > maxVilage){
+                maxVilage = k;
+                idx = k;
+            }
+        }
+
+        System.out.println(idx + " " + maxVilage);
+    }
+}
