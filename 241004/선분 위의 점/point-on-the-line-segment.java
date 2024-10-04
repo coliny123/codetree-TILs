ecee0c018f0a15c3865b0e055d6e40bdbc761cab
김도환 <87745117+coliny123@users.noreply.github.com>
2024-10-04 16:37:03 +0900

    Title: 선분 위의 점, Time: 2681ms, Memory: 93MB, Status: Wrong Answer - CodeTree

---
+++ 241004/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+// class Node implements Comparable<Node>{
+//     int cnt, value;
+    
+//     public Node(int cnt, int value){
+//         this.cnt=cnt;
+//         this.value=value; 
+//     }
+
+//     @Overrdie
+//     public int compareTo(Node x){
+        
+//     }
+// }
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i=0; i<N; i++){
+            set.add(sc.nextInt());
+        }
+        set.add(1000000001);
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int cnt=1;
+        for(int num : set){
+            map.put(num, cnt);
+            cnt++;
+        }
+        // int st = 1;
+        // int ed = 5;
+        // System.out.println(set.ceiling(st) + " " + set.higher(ed));
+
+        while(M-- > 0){
+            int st = sc.nextInt();
+            int ed = sc.nextInt();
+
+            int newA = map.get(set.ceiling(st));
+            int newB = map.get(set.higher(ed));
+
+            System.out.println(newB - newA);
+        }
+        
+    }
+}
