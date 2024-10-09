b96008bf0ba6ea613d099a15f53b7b7635a5275e
김도환 <87745117+coliny123@users.noreply.github.com>
2024-10-10 01:04:06 +0900

    Title: 원소의 합이 0, Time: 635ms, Memory: 33MB, Status: Passed - CodeTree

--- 241010/원소의 합이 0/the-sum-of-the-elements-is-0.java
+++ 241010/원소의 합이 0/the-sum-of-the-elements-is-0.java
@@ -9,30 +9,30 @@ public class Main {
 
         int[][] nums = new int[4][N];
 
-        Set<Integer> set = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<4; i++){
             for(int j=0; j<N; j++){
                 nums[i][j] = sc.nextInt();
             }
         }
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++) {
                 int key = nums[0][i] + nums[1][j];
-                set.add(key);
+                map.put(key, map.getOrDefault(key, 0) + 1); // map을 쓰는 이유 : A[i] + B[j] 가 같은 조합이 여러개 나올 수 있기 때문에 그 것을 카운트해야됨
+            }
+        } 
+
+        int ans=0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++) {
+                int diff = - nums[2][i] - nums[3][j];
+                if(map.getOrDefault(diff, 0) > 0)  // map에 -(C[i] + D[j])가 존재하는지 체크, 없으면 0을 리턴 -> 0이면 패스
+                    ans += map.get(diff);
             }
         }
 
-        int cnt = 0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                int remain = -(nums[2][i] + nums[3][j]);
-                if(set.contains(remain)){
-                    cnt++;
-                }
-            }
-        }
-
-        System.out.println(cnt);
+        System.out.println(ans);
         
     }
 }
