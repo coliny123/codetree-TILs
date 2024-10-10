e1cbd4883efaa6903306d24920fbaed4fbf30d8d
김도환 <87745117+coliny123@users.noreply.github.com>
2024-10-10 09:17:04 +0900

    Title: 행복한 수열의 개수, Time: 273ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 241010/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int m;
+
+    public static int grid[][];
+
+    public static int seq[];
+
+    public static boolean isHappySequence(){
+        int consecutiveCount = 1, maxCcnt=1;
+        for(int i=1; i<n; i++){
+            if(seq[i-1] == seq[i])
+                consecutiveCount++;
+            else
+                consecutiveCount = 1;
+
+            maxCcnt = Math.max(maxCcnt, consecutiveCount);
+        }
+
+        return maxCcnt >= m;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int answer = 0;
+
+        grid = new int[n][n];
+        seq = new int[n];
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            int seqCnt=1;
+            for(int j=1; j<n; j++){
+                if(grid[i][j-1]==grid[i][j]){
+                    seqCnt++;
+                }
+            }
+            if(seqCnt >= m)
+                answer++;
+        }
+
+        for(int i=0; i<n; i++){
+            int seqCnt=1;
+            for(int j=1; j<n; j++){
+                if(grid[j-1][i] == grid[j][i])
+                    seqCnt++;
+            }
+            if(seqCnt >= m)
+                answer++;
+        }
+
+        System.out.println(answer);
+
+    }
+}
